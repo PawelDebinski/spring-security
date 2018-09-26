1a399d7ab4a662e36cb72c9b346f53b5134503fd
Pawel Debinski <spokojasne@interia.eu>
2018-09-26 20:40:16 +0200

    init commit

---
+++ src/main/java/pl/pawel/springsecurity/security/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package pl.pawel.springsecurity.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+    // special class to register security filters
+}
